b70d2f384f6d6633d7d7b1d27f576dfaa6cff869
Andrew W Burnett <burnett.w.andrew@gmail.com>
2025-02-01 15:56:52 +0000

    CHANGELOG
    === ORGANISATIONAL CHANGES ===
    - None.
    
    === CMAKE CHANGES ===
    Removed:
    include/abc_test/included_instances/data_generator/enumeration/specializations/fundamental_types.hpp
    include/abc_test/included_instances/data_generator/enumeration/specializations/enum.hpp
    include/abc_test/included_instances/data_generator/enumeration/specializations/array.hpp
    include/abc_test/included_instances/data_generator/enumeration/specializations/vector.hpp
    include/abc_test/included_instances/data_generator/enumeration/specializations.hpp
    
    === LIBRARY CHANGES ===
    - None.
    
    == FILE ORGANISATION CHANGES ==
    - None.
    
    == CODE CHANGES ==
    - None.
    
    == NAMESPACE CHANGES ==
    - None.
    
    == MACRO CHANGES ==
    - None
    
    == FORMATTING CHANGES ==
    - None.
    
    === TESTING CHANGES ===
    - None
    
    === DOCUMENTATION CHANGES ===
    - None.
    
    === EXAMPLES CHANGES ===
    - None.

--- library/include/abc_test/included_instances/data_generator/enumeration/specializations.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "abc_test/included_instances/data_generator/enumeration/specializations/array.hpp"
-#include "abc_test/included_instances/data_generator/enumeration/specializations/enum.hpp"
-#include "abc_test/included_instances/data_generator/enumeration/specializations/fundamental_types.hpp"
-#include "abc_test/included_instances/data_generator/enumeration/specializations/vector.hpp"
