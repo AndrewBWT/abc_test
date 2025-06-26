f546695bcbc2e2b5b12e488acb552732d88526ee
Andrew W Burnett <burnett.w.andrew@gmail.com>
2025-06-26 15:02:43 +0100

    CHANGELOG
    === CODE CHANGES ===
    # This section describes any changes to the underlying code or library.
    
    === TESTING CHANGES ===
    # This section describes any changes to the testing folder.
    
    === DOCUMENTATION CHANGES ===
    # This section describes any changes to the documentation folder.
    
    === EXAMPLES CHANGES ===
    # This section describes any changes to the examples folder.
    
    === OTHER CHANGES ===
    # This section describes: organisational changes, clang-format changes.
    - Removed an incorrect line from the README generation file.

--- docs/scripts/pre_comit_check_docs.sh
+++ docs/scripts/pre_comit_check_docs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "🔧 Regenerating documentation from source..."
-rm README_pc.md
+rm README.md
 python docs/scripts/inject_code.py docs/user_written_docs/README.md --out README.md
-git add README_pc.md
+git add README.md
 echo "✅ Documentation generation is complete"
